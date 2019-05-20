dfebdcd1c052668500e12b8b4da9386e45da2771
optimistic-creator-7568 <alexandro.alanya@gmail.com>
2019-05-20 20:43:04 +0000

    Done.

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -1,4 +1,6 @@
-describe '#speak_to_grandma' do
+if "speak to grandma"
+  puts "HUH?! SPEAK UP, SONNY!"
+
   it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
     expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
   end
